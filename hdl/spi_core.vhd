076ddc81046c9cfa2f2ffb588b9248f697075d7e
shhx <luisgomezlo.lg@gmail.com>
2025-01-12 02:43:05 +0100

    Fixed counter sizes

--- hdl/spi_core.vhd
+++ hdl/spi_core.vhd
@@ -85,14 +85,15 @@ ARCHITECTURE Behavioral OF spi_core IS
 
     -- SPI Clock signals
     SIGNAL clk_count : INTEGER RANGE 0 TO 2 ** 16 - 1 := 0;
-    SIGNAL wait_count : INTEGER RANGE 0 TO 2 ** 16 - 1 := 0;
+    SIGNAL wait_count : unsigned(long_wait_cycles'LENGTH - 1 DOWNTO 0) := (OTHERS => '0');
     SIGNAL spi_clk : STD_LOGIC := '0';
-    SIGNAL spi_clk_toggles : INTEGER RANGE 0 TO MAX_NUMBER_READS * 8 - 1 := 0;
-    SIGNAL bits_to_transfer : INTEGER RANGE 0 TO 8 * MAX_NUMBER_READS := 0;
-    SIGNAL last_bit : INTEGER RANGE 0 TO 8 * MAX_NUMBER_READS := 0;
+    SIGNAL spi_clk_toggles : INTEGER RANGE 0 TO MAX_NUMBER_READS * 8 * 2 := 0;
+    SIGNAL bits_to_transfer : INTEGER RANGE 0 TO MAX_NUMBER_READS * 8 := 0;
+    SIGNAL last_bit : INTEGER RANGE 0 TO MAX_NUMBER_READS * 8 * 2 := 0;
     SIGNAL chip_select : STD_LOGIC := '1';
     SIGNAL prev_transfer_inhibit : STD_LOGIC := '1';
     SIGNAL receive_transmit : STD_LOGIC := '0'; --'1' for tx, '0' for rx 
+    SIGNAL receive_transmit_ff : STD_LOGIC := '0';
     SIGNAL mosi_ff : STD_LOGIC := '0';
 
     -- SPI Data signals
@@ -111,8 +112,6 @@ ARCHITECTURE Behavioral OF spi_core IS
     TYPE fsm_state IS (IDLE, CS_LOW_WAIT, TRANSFER, CS_HIGH_WAIT, LONG_WAIT);
     SIGNAL state : fsm_state := IDLE;
 BEGIN
-    bits_to_transfer <= to_integer(unsigned(xfer_count)) * 8;
-
     sck <= spi_clk;
     xfer_error <= axi_transfer_error;
     rx_full <= spi_transfer_done AND NOT axis_transfer_done;
@@ -121,14 +120,15 @@ BEGIN
     PROCESS (clk, rst)
         VARIABLE SPI_CLK_CYCLES : INTEGER := 0;
     BEGIN
-        last_bit <= bits_to_transfer * 2;
         SPI_CLK_CYCLES := to_integer(unsigned(clk_div));
         IF rst = '0' THEN
-            wait_count <= 0;
+            wait_count <= (OTHERS => '0');
             spi_clk_toggles <= 0;
             spi_transfer_done <= '0';
             state <= IDLE;
         ELSIF rising_edge(clk) THEN
+            bits_to_transfer <= to_integer(unsigned(xfer_count)) * 8;
+            last_bit <= bits_to_transfer * 2;
             prev_transfer_inhibit <= transfer_inhibit;
             IF transfer_inhibit = '1' THEN
                 state <= IDLE;
@@ -144,15 +144,15 @@ BEGIN
                     mosi_count <= 0;
                     miso_count <= 0;
                     IF (prev_transfer_inhibit = '1' AND transfer_inhibit = '0') OR (automatic_transfers = '1' AND transfer_inhibit = '0') THEN
-                        wait_count <= 0;
+                        wait_count <= (OTHERS => '0');
                         state <= CS_LOW_WAIT;
                     END IF;
 
                 WHEN CS_LOW_WAIT =>
                     chip_select <= '0';
                     spi_transfer_done <= '0';
-                    IF wait_count = CS_WAIT_CYCLES THEN
-                        wait_count <= 0;
+                    IF wait_count = to_unsigned(CS_WAIT_CYCLES, wait_count'LENGTH) THEN
+                        wait_count <= (OTHERS => '0');
                         state <= TRANSFER;
                     ELSE
                         wait_count <= wait_count + 1;
@@ -194,8 +194,8 @@ BEGIN
                     END IF;
 
                 WHEN CS_HIGH_WAIT =>
-                    IF wait_count = CS_WAIT_CYCLES THEN
-                        wait_count <= 0;
+                    IF wait_count = to_unsigned(CS_WAIT_CYCLES, wait_count'LENGTH) THEN
+                        wait_count <= (OTHERS => '0');
                         IF automatic_transfers = '1' THEN
                             state <= LONG_WAIT;
                         ELSE
@@ -209,8 +209,8 @@ BEGIN
                     chip_select <= '1';
                     spi_clk <= '0';
                     clk_count <= 0;
-                    IF wait_count = to_integer(unsigned(long_wait_cycles)) THEN
-                        wait_count <= 0;
+                    IF wait_count = unsigned(long_wait_cycles) THEN
+                        wait_count <= (OTHERS => '0');
                         state <= IDLE;
                     ELSE
                         wait_count <= wait_count + 1;
@@ -289,7 +289,6 @@ BEGIN
             axi_transfer_error <= '0';
             result_index <= 0;
         ELSIF rising_edge(s_axis_out_aclk) THEN
-            prev_spi_transfer_done <= spi_transfer_done;
             IF spi_transfer_done = '1' AND axis_transfer_done = '0' THEN
                 IF result_index < N_SENSORS THEN
                     s_axis_out_tdata <= zeros & sensor_data(result_index);
@@ -335,9 +334,16 @@ BEGIN
         -- Set the desired chip's select to the given value, use one-hot encoding.
         -- Bits set to '0' will be selected.
         FOR i IN 0 TO N_CHIP_SELECTS - 1 LOOP
-            IF selected_cs(i) = '0' THEN
-                cs(i) <= chip_select;
+            IF automatic_transfers = '1' THEN
+                IF selected_cs(i) = '0' THEN
+                    cs(i) <= chip_select;
+                END IF;
+            ELSE
+                IF selected_cs(i) = '0' THEN
+                    cs(i) <= selected_cs(i);
+                END IF;
             END IF;
+
         END LOOP;
     END PROCESS;
 
